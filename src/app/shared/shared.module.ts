053597449054f8c746e1f2ccaadd2f31c9828f69
EverNavarro <ever.navarro@globant.com>
2023-09-04 22:35:38 -0500

    remove unused

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TestSharedComponent } from './components/test-shared/test-shared.component';
 import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    TestSharedComponent,
     CardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    TestSharedComponent,
     CardComponent
   ]
 })
